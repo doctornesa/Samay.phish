1f65a1dfb9bcba06d4bd8da348fd36632f0a1d44
samay826 <71082274+samay826@users.noreply.github.com>
2020-09-12 09:40:49 +0530

    Update S-Phish.sh

--- S-Phish.sh
+++ S-Phish.sh
@@ -464,6 +464,5 @@ printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;96m Saved:\e[0m\e[1;93m 
 printf "\n"
 printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;93m Waiting for Next Login Info,\e[0m\e[1;96m Ctrl + C to exit.\e[0m\n"
 }
-banner
 deps
 menu
